cabd1d2e0e1e84824455dd1a6d6c67799f6b6107
marhaka <marhaka@kth.se>
2023-01-12 17:28:27 +0100

    added content

--- config/database.js
+++ config/database.js
@@ -1,3 +1,19 @@
+/*
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('PGHOST', 'localhost'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'postgres'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'insertPW'),
+      ssl: env.bool(false),
+    },
+  },
+});
+*/
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
